81593ad0cc73647bbc89b7ecdc42d14ccbfb1a68
citakilkay <citakilkay@gmail.com>
2023-02-23 16:26:27 +0300

    localization

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { Suspense } from "react";
+import { useTranslation } from "react-i18next";
+
 function App() {
+  const { t } = useTranslation()
   return (
     <>
+      <Suspense fallback={null}>
+        {t('product_list')}
+      </Suspense>
     </>
   );
 }
--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,6 +1,44 @@
 import axios from 'axios'
+import 'dotenv/config.js';
+import { Modal } from 'antd';
 
 const http = axios.create({
-    baseURL: AppConst.remoteServiceBaseUrl,
+    baseURL: process.env.SERVER_APP_URL,
     timeout: 30000
 });
+
+http.interceptors.request.use(
+    function (config) {
+        //Middlewares for request will be added in this scope
+        // Authorization and other common headers will be added in this scope
+        return config
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+)
+
+http.interceptors.response.use(
+    response => {
+        //Middlewares for response will be added in this scope
+        return response;
+    },
+    error => {
+        //Middlewares for error will be added in this scope
+        if (!!error.response && !!error.response.data.error && !!error.response.data.error.message && error.response.data.error.details) {
+
+            Modal.error({
+                title: error.response.data.error.message,
+                content: error.response.data.error.details,
+            });
+        }
+        else if (!error.response) {
+            Modal.error({ content: 'Bilinmeyen Hata.' });
+        }
+
+        setTimeout(() => {
+            return Promise.reject(error);
+        }, 1000);
+    }
+)
+
